3547a680241c292889c68cf29772cf61bd9442a6
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-04-25 21:27:04 +0530

    bugs fixed

--- Project/Apis.js
+++ Project/Apis.js
@@ -53,8 +53,8 @@ dotenv.config();
 const transporter = mailer.createTransport({
     service: "gmail",
     auth: {
-        user: process.env.EMAIL_USER,      
-        pass: process.env.EMAIL_PASS
+        user: process.env.EMAIL_USER,    //stored in .env file  
+        pass: process.env.EMAIL_PASS     //stored in .env file
           
     }
 });
